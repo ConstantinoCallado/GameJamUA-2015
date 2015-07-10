184a8e758ac1ff09b3a8ac1d73006001447cd716
Luisetepe <luisetepe6@gmail.com>
2015-07-10 21:39:58 +0200

    Merge que te crió

---
+++ GameJamUA-2015/Assets/Scripts/Vaso.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vaso : MonoBehaviour {
+
+    private Vector3 initialCm;
+
+	// Use this for initialization
+	void Start () {
+        initialCm = GetComponent<Rigidbody>().centerOfMass;
+        GetComponent<Rigidbody>().centerOfMass += (Vector3.one * 0.108f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Mathf.Abs(transform.rotation.x) > 80)
+        {
+            GetComponent<Rigidbody>().centerOfMass = initialCm;
+            Debug.Log("SFFSDFSDFSD");
+        }
+	}
+}
