c57124f3de76084a15bf548d58d95a642a627304
Luisetepe <luisetepe6@gmail.com>
2015-07-10 19:37:33 +0200

    Prototipo cocinero

---
+++ GameJamUA-2015/Assets/Scripts/CocineroBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CocineroBehaviour : MonoBehaviour {
+
+    private bool cambioGiro = false;
+    Salchicha player = Salchicha.playerRef;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+        foreach(Transform centinela in GetComponentsInChildren<Transform>())
+        {
+            Debug.DrawRay(transform.position, (centinela.position - transform.position), Color.red);
+        }
+
+        if (!cambioGiro)
+        {
+            transform.Rotate(Vector3.up, 1f);
+            if (transform.eulerAngles.y >= 88f && transform.eulerAngles.y <= 92f)
+                cambioGiro = true;
+        }
+        else       //Umbrales sucios para detectar cambio de direccion  
+        {
+            transform.Rotate(Vector3.up, -1f);
+            if (transform.eulerAngles.y >= 268f && transform.eulerAngles.y <= 272f)
+                cambioGiro = false;
+        }
+
+        if (player.transform.position.z > GameObject.Find("CentinelaIzq").transform.position.z &&
+            player.transform.position.z < GameObject.Find("CentinelaDer").transform.position.z)
+            Debug.Log("MUERE!!!!");
+	}
+}
