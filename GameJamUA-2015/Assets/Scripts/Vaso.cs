209382f9db1129786f894c5e7578669942ccdb09
Luisetepe <luisetepe6@gmail.com>
2015-07-11 01:20:57 +0200

    Nuevo Vaso

--- GameJamUA-2015/Assets/Scripts/Vaso.cs
+++ GameJamUA-2015/Assets/Scripts/Vaso.cs
@@ -5,7 +5,7 @@ public class Vaso : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        GetComponent<Rigidbody>().centerOfMass += (Vector3.one * 0.109f);
+        GetComponent<Rigidbody>().centerOfMass += (Vector3.one * 0.05f);
 	}
 	
 	// Update is called once per frame
