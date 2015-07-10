86cb0f3124cfc9ab198390eaca3ada1e2bdb2081
constan <constantinocalladoperez@gmail.com>
2015-07-10 19:37:10 +0200

    Salchicha basica creada

---
+++ GameJamUA-2015/Assets/Scripts/Salchicha.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Salchicha : MonoBehaviour 
+{
+	public Rigidbody rigidBodyDerecha;
+	public Rigidbody rigidBodyIzquierda;
+
+	public float fuerza = 5;
+
+	void Update () 
+	{
+		if(Input.GetKey(KeyCode.A))
+		{
+			rigidBodyIzquierda.AddForce(new Vector3(0, 0, -fuerza));
+		}
+		else if(Input.GetKey(KeyCode.D))
+		{
+			rigidBodyIzquierda.AddForce(new Vector3(0, 0, fuerza));
+		}
+		if(Input.GetKey(KeyCode.W))
+		{
+			rigidBodyIzquierda.AddForce(new Vector3(fuerza, 0, 0));
+		}
+		else if(Input.GetKey(KeyCode.S))
+		{
+			rigidBodyIzquierda.AddForce(new Vector3(-fuerza, 0, 0));
+		}
+
+
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			rigidBodyDerecha.AddForce(new Vector3(0, 0, -fuerza));
+		}
+		else if(Input.GetKey(KeyCode.RightArrow))
+		{
+			rigidBodyDerecha.AddForce(new Vector3(0, 0, fuerza));
+		}
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			rigidBodyDerecha.AddForce(new Vector3(fuerza, 0, 0));
+		}
+		else if(Input.GetKey(KeyCode.DownArrow))
+		{
+			rigidBodyDerecha.AddForce(new Vector3(-fuerza, 0, 0));
+		}
+	}
+}
