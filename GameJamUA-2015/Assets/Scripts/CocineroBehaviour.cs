0e97a1a5b5a4e3c9fbb898ef0e483e68465de861
constan <constantinocalladoperez@gmail.com>
2015-07-11 21:28:25 +0200

    Refactorizado el cocinero

--- GameJamUA-2015/Assets/Scripts/CocineroBehaviour.cs
+++ GameJamUA-2015/Assets/Scripts/CocineroBehaviour.cs
@@ -4,21 +4,22 @@ using System.Collections;
 public class CocineroBehaviour : MonoBehaviour {
 
     private bool cambioGiro = false;
-    public float velocidadGiro = 0.5f;
-	
+    public float velocidadGiro = 30f;
+	public float cantidadGiro = 90;
+
 	// Update is called once per frame
 	void Update () {
 
         if (!cambioGiro)
         {
-            transform.Rotate(Vector3.up, velocidadGiro);
-            if (transform.eulerAngles.y >= 88f && transform.eulerAngles.y <= 92f)
+            transform.Rotate(Vector3.up, velocidadGiro * Time.deltaTime);
+			if (transform.eulerAngles.y >= cantidadGiro -2  && transform.eulerAngles.y <= cantidadGiro + 2)
                 cambioGiro = true;
         }
         else       //Umbrales sucios para detectar cambio de direccion  
         {
-            transform.Rotate(Vector3.up, -velocidadGiro);
-            if (transform.eulerAngles.y >= 268f && transform.eulerAngles.y <= 272f)
+			transform.Rotate(Vector3.up, -velocidadGiro * Time.deltaTime);
+			if (transform.eulerAngles.y >= (360 - cantidadGiro) - 2 && transform.eulerAngles.y <= (360 - cantidadGiro) + 2)
                 cambioGiro = false;
         }
 
