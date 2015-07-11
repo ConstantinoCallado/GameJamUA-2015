76891e51234261b85a544090d15bb7489889b1f7
Luisetepe <luisetepe6@gmail.com>
2015-07-11 22:38:02 +0200

    Carga de niveles implementada

---
+++ GameJamUA-2015/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public static void LoadLevel()
+    {
+        int nivelActual = Application.loadedLevel;
+
+        if (nivelActual < 3)
+            Application.LoadLevel(nivelActual + 1);
+        else
+        {
+            //Finalizar juego
+        }
+    }
+}
