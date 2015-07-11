03a1b26a7f0e71fad3e668db8483069f45448475
constan <constantinocalladoperez@gmail.com>
2015-07-11 19:59:30 +0200

    La cinematica de chafar al jugador ya funciona

--- GameJamUA-2015/Assets/Scripts/Centinela.cs
+++ GameJamUA-2015/Assets/Scripts/Centinela.cs
@@ -3,14 +3,32 @@ using System.Collections;
 
 public class Centinela : MonoBehaviour 
 {
+	public CinematicaChafar prefabCinematica;
+	public static float cooldown;
+
     public void OnTriggerEnter(Collider other)
     {
-        if (other.transform.tag == "Salchicha")
+		if (other.transform.tag == "Salchicha" && Time.time > cooldown)
         {
-            if (other.GetComponent<Rigidbody>().velocity.sqrMagnitude >= 0.2f)
-                {
-                    Debug.Log("PILLADA!!!!");
-                }
+            if (other.GetComponent<Rigidbody>().velocity.sqrMagnitude >= 0.1f)
+            {
+				cooldown = Time.time + 7;
+				StartCoroutine(corutinaCinematica());
+                Debug.Log("PILLADA!!!!");
+            }
         }
     }
+
+	public IEnumerator corutinaCinematica()
+	{
+		Camera cameraOld = Camera.main;
+		Salchicha.playerRef.showUI = false;
+		GameObject instanciado = GameObject.Instantiate(prefabCinematica.gameObject);
+		instanciado.transform.position = new Vector3(900, 900, 900);
+		instanciado.GetComponent<CinematicaChafar>().Show(3);
+		yield return new WaitForSeconds(3);
+
+		Destroy(instanciado);
+		Salchicha.playerRef.Kill();
+	}
 }
---
+++ GameJamUA-2015/Assets/Scripts/CinematicaChafar.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CinematicaChafar : MonoBehaviour 
+{
+	public static CinematicaChafar cinematicaChafarRef;
+	public Camera camaraCinematica;
+	public Animation animationChef;
+
+	public void Show(float segundos)
+	{
+		StartCoroutine(corutinaChafar(segundos));
+	}
+
+	public IEnumerator corutinaChafar(float segundos)
+	{
+		animationChef.Play();
+		//Camera.SetupCurrent(camaraCinematica);
+
+		yield return new WaitForSeconds(segundos);
+	}
+}
