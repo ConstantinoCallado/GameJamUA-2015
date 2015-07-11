1bdc8a924b8526a0a11b227a4993c9084596a5a8
constan <constantinocalladoperez@gmail.com>
2015-07-12 01:04:14 +0200

    Introduccion de la nueva barra de toxicidad Nivelada la friccion para conseguir un movimiento mas agil Se ha corregido el cono de vision del cocinero

--- GameJamUA-2015/Assets/Scripts/LevelManager.cs
+++ GameJamUA-2015/Assets/Scripts/LevelManager.cs
@@ -1,17 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LevelManager : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+public class LevelManager : MonoBehaviour 
+{
+	public int[] toxicidades = {10, 10, 10, 10};
 
     public static void LoadLevel()
     {
