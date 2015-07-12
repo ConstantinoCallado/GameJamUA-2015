e861168902fb7ef11e0a795a54c200ca454fde6a
Luisetepe <luisetepe6@gmail.com>
2015-07-12 13:20:26 +0200

    Coin system

---
+++ GameJamUA-2015/Assets/Scripts/Coin.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour {
+
+    public void OnTriggerEnter(Collider other)
+    {
+        
+
+        if (other.transform.tag == "Salchicha")
+        {
+            switch (Application.loadedLevel)
+            {
+                case 1: PlayerPrefs.SetInt("C1", 1);
+                    break;
+                case 2:PlayerPrefs.SetInt("C2", 1);
+                    break;
+                case 3: PlayerPrefs.SetInt("C3", 1);
+                    break;
+                case 4: PlayerPrefs.SetInt("C4", 1);
+                    break;
+
+            }
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ GameJamUA-2015/Assets/Scripts/Inicio.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inicio : MonoBehaviour {
+
+	
+	void Awake () {
+
+        if (!(PlayerPrefs.HasKey("C1") && PlayerPrefs.HasKey("C2") && PlayerPrefs.HasKey("C3") && PlayerPrefs.HasKey("C4")))
+        {
+            PlayerPrefs.SetInt("C1", 0);
+            PlayerPrefs.SetInt("C2", 0);
+            PlayerPrefs.SetInt("C3", 0);
+            PlayerPrefs.SetInt("C4", 0);
+        }
+	}
+	
+
+}
