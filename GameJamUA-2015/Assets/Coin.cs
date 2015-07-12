3f0891ca3d7c0a1caf070d9e4ddd813e39c7bf89
Luisetepe <luisetepe6@gmail.com>
2015-07-12 12:51:53 +0200

    Pusheikoh

--- GameJamUA-2015/Assets/Coin.cs
+++ GameJamUA-2015/Assets/Coin.cs
@@ -1,13 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Cable : MonoBehaviour {
+public class Coin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "Salchicha")
         {
-            Salchicha.playerRef.Electrocutar();
+            
         }
     }
 }
